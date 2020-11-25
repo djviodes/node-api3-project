d2d2794830ca0fa4e754951653369e47d729a186
djviodes <djviodes26@gmail.com>
2020-11-24 20:47:00 -0800

    Finished MVP

--- README.md
+++ README.md
@@ -27,22 +27,22 @@ Follow these steps to set up and work on your project:
   [X] `logger` logs to the console the following information about each request: request method, request url, and a timestamp
   [X] this middleware runs on every request made to the API
 
-[] `validateUserId()`
+[X] `validateUserId()`
 
-  [] this middleware will be used for all endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id. If there is no user with that id return HTTP status code 404 and a useful error message. If a user with that id is found, then let the request continue.
-  [] if the `id` parameter is valid, store that user object as `req.user`
-  [] if the `id` parameter does not match any user id in the database, respond with status `400` and `{ message: "invalid user id" }`
+  [X] this middleware will be used for all endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id. If there is no user with that id return HTTP status code 404 and a useful error message. If a user with that id is found, then let the request continue.
+  [X] if the `id` parameter is valid, store that user object as `req.user`
+  [X] if the `id` parameter does not match any user id in the database, respond with status `400` and `{ message: "invalid user id" }`
 
-[] `validateUser()`
+[X] `validateUser()`
 
-  [] `validateUser` validates the `body` on a request to create a new user
-  [] if the request `body` is missing, respond with status `400` and `{ message: "missing user data" }`
-  [] if the request `body` lacks the required `name` field, respond with status `400` and `{ message: "missing required name field" }`
+  [X] `validateUser` validates the `body` on a request to create a new user
+  [X] if the request `body` is missing, respond with status `400` and `{ message: "missing user data" }`
+  [X] if the request `body` lacks the required `name` field, respond with status `400` and `{ message: "missing required name field" }`
 
-[] `validatePost()`
-  [] `validatePost` validates the `body` on a request to create a new post
-  [] if the request `body` is missing, respond with status `400` and `{ message: "missing post data" }`
-  [] if the request `body` lacks the required `text` field, respond with status `400` and `{ message: "missing required text field" }`
+[X] `validatePost()`
+  [X] `validatePost` validates the `body` on a request to create a new post
+  [X] if the request `body` is missing, respond with status `400` and `{ message: "missing post data" }`
+  [X] if the request `body` lacks the required `text` field, respond with status `400` and `{ message: "missing required text field" }`
 
 ### Database Persistence Helpers
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,150 @@
 const express = require('express');
 
+const Hubs = require('./userDb');
+const Posts = require('../posts/postDb');
+
 const router = express.Router();
-
-router.post('/', (req, res) => {
-  // do your magic!
-});
-
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.get('/', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  Hubs.getById(id)
+    .then(data => {
+      if (data) {
+        req.hub = data
+        next()
+      } else {
+        next({ code: 400, message: 'invalid user id' })
+      }
+    })
+    .catch(err => {
+      next({ code: 500, message: err })
+    })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(req.body === {}) {
+    next({ code: 400, message: 'missing user data' })
+  } else if (!req.body.name) {
+    next({ code: 400, message: 'missing required name field' })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(req.body === {}) {
+    next({ code: 400, message: 'missing post data' })
+  } else if (!req.body.text) {
+    next({ code: 400, message: 'missing required text field' })
+  } else {
+    next()
+  }
 }
+
+router.post('/', validateUser, (req, res) => {
+  Hubs.insert(req.body)
+    .then(data => {
+      const newUser = { ...data, ...req.body }
+      console.log(newUser)
+      res.status(201).json(req.body)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.post('/:id/posts', validatePost, (req, res) => {
+  Posts.insert(req.body)
+    .then(data => {
+      res.status(201).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.get('/', (req, res) => {
+  Hubs.get(req.query)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.hub)
+});
+
+router.get('/:id/posts', (req, res) => {
+  Hubs.getUserPosts(req.params.id)
+    .then(data => {
+      if (!data.length) {
+        res.status(404).json({
+          message: 'Post not found'
+        });
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Hubs.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `User ${req.params.id} has been successfully deleted`
+        });
+      } else {
+        res.status(404).json({
+          message: 'User not found'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+    .then(data => {
+      if (data) {
+        const updateUser = { id: req.params.id, ...changes }
+        res.status(200).json(updateUser);
+      } else {
+        res.status(400).json({
+          message: 'You must have goofed something up. Mickey does NOT approve...'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.code).json({ message: err.message });
+});
 
 module.exports = router;

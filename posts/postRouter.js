2e89e098188877ac767d4870fa9e7a33a8ee82f7
djviodes <djviodes26@gmail.com>
2020-11-24 19:12:04 -0800

    Finished the logger custom middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,54 @@
 const express = require('express');
 
+const Hubs = require('./postDb');
+
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  Hubs.getById(id)
+    .then(data => {
+      if (data) {
+        req.hub = data
+        next()
+      } else {
+        next({ code: 400, message: 'There is no hub with id ' + id })
+      }
+    })
+    .catch(error => {
+      next({ code: 500, message: error })
+    })
 }
+
+router.get('/', (req, res) => {
+  Hubs.get(req.query)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message,
+        stack: err.stack
+      });
+    });
+});
+
+router.get('/:id', validatePostId, (req, res) => {
+  // do your magic!
+});
+
+router.delete('/:id', validatePostId, (req, res) => {
+  // do your magic!
+});
+
+router.put('/:id', validatePostId, (req, res) => {
+  // do your magic!
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.code).json({ message: err.message });
+});
 
 module.exports = router;
